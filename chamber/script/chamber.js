a5c65d8c5f40ca17e31f9679859a782bb12c9090
kevingonmed <kevingonmed@gmail.com>
2023-10-24 21:23:04 -0600

    dark mode and align the content

--- chamber/script/chamber.js
+++ chamber/script/chamber.js
@@ -21,3 +21,19 @@ fetch(weatherUrl)
     .catch((error) => {
         console.error('Error fetching weather data:', error);
     });
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#274c77";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
